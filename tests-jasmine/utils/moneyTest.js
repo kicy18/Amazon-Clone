ee7041405852bb459bcbd7fdd5b07341cae722cd
kicy <80158936+kicy18@users.noreply.github.com>
2024-09-18 15:50:15 +0530

    Create automated Tests fro addtocart

--- tests-jasmine/utils/moneyTest.js
+++ tests-jasmine/utils/moneyTest.js
@@ -1,4 +1,4 @@
-import {formatCurrency} from '../scripts/utils/money.js';
+import {formatCurrency} from '../../scripts/utils/money.js';
 
 describe('Test Suite : Foramtcurrency',()=>{
     it('convert cents into dollars', ()=>{
